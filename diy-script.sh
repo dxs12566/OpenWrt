3f9d3f722e847951a8e54e3e519e5454bd485077
dxs1256 <dxs1256@gmail.com>
2024-02-06 12:02:37 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -35,13 +35,14 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 
 
 # 添加aliyundrive-webdav
-# rm -rf feeds/packages/multimedia/aliyundrive-webdav
-# rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
-# git clone https://github.com/messense/aliyundrive-webdav.git
-# git_sparse_clone master https://github.com/haiibo/openwrt-packages openwrt-packages/webdav2
-# cp -r aliyundrive-webdav/openwrt/aliyundrive-webdav feeds/packages/multimedia
-# cp -r aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications
-# rm -rf aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+git clone https://github.com/messense/aliyundrive-webdav.git
+git_sparse_clone master https://github.com/haiibo/openwrt-packages webdav2
+cp -r webdav2 feeds/packages/multimedia
+cp -r aliyundrive-webdav/openwrt/aliyundrive-webdav feeds/packages/multimedia
+cp -r aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications
+rm -rf aliyundrive-webdav
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
